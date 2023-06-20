822d4471e588d8e6a36dd15b66eeddb420cb8e5e
student <swanjikumaina@gmail.com>
2023-06-20 08:44:52 +0300

    registration page

---
+++ app/src/main/java/dev/wanjiku/billz/MainActivity.kt
@@ -0,0 +1,76 @@
+package dev.wanjiku.billz
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import dev.wanjiku.billz.databinding.ActivityRegistrationBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+//        private var binding: ActivityRegistrationBinding? = null
+//        override fun onCreate(savedInstanceState: Bundle?) {
+//            super.onCreate(savedInstanceState)
+//            binding = ActivityRegistrationBinding.inflate(layoutInflater)
+//            setContentView(binding!!.getRoot())
+//            binding!!.btnRegister.setOnClickListener(View.OnClickListener {
+//
+//                val username: String = binding!!.teUserName.getText().toString().trim()
+//                val phoneNumber: String = binding!!.tePhoneNumber.getText().toString().trim()
+//                val email: String = binding!!.teEmail.getText().toString().trim()
+//                val password: String = binding!!.tePassword.getText().toString().trim()
+//
+
+            }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/dev/wanjiku/billz/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package dev.wanjiku.billz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
